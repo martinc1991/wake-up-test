4a06a304601b92092f605fcc660a4232817c01b3
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-05-06 12:00:54 -0300

    Cleanup products code

--- apps/api/src/products/products.controller.ts
+++ apps/api/src/products/products.controller.ts
@@ -1,8 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
-import { CreateProductDto } from './dto/create-product.dto'
-import { UpdateProductDto } from './dto/update-product.dto'
-import { ProductsService } from './products.service'
+import { Controller, Get, Param } from '@nestjs/common'
 import { FindManyProductsResponse, FindOneProductResponse } from 'contract'
+import { ProductsService } from './products.service'
 
 @Controller('products')
 export class ProductsController {
@@ -16,20 +14,5 @@ export class ProductsController {
   @Get(':id')
   findOne(@Param('id') id: string): Promise<FindOneProductResponse | null> {
     return this.productsService.findOne(id)
-  }
-
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto)
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto)
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productsService.remove(id)
   }
 }
--- apps/api/src/products/products.service.ts
+++ apps/api/src/products/products.service.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@nestjs/common'
-import { CreateProductDto } from './dto/create-product.dto'
-import { UpdateProductDto } from './dto/update-product.dto'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
-  create(createProductDto: CreateProductDto) {
-    return this.prisma.product.create({ data: createProductDto })
-  }
 
   findAll() {
     return this.prisma.product.findMany()
@@ -16,16 +11,5 @@ export class ProductsService {
 
   findOne(id: string) {
     return this.prisma.product.findUniqueOrThrow({ where: { id } })
-  }
-
-  update(id: string, updateProductDto: UpdateProductDto) {
-    return this.prisma.product.update({
-      where: { id },
-      data: updateProductDto,
-    })
-  }
-
-  remove(id: string) {
-    return this.prisma.product.delete({ where: { id } })
   }
 }
