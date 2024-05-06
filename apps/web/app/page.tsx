9dd06c9cb7bef74637365895099f2f9d1f107da9
MartinC <59508264+martinc1991@users.noreply.github.com>
2024-05-06 12:20:58 -0300

    Change sign in title component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,14 @@
 import { FlexDiv } from '@/components/flex-div'
 import { SignInForm } from '@/components/sign-in-form'
 import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Typography } from '@/components/ui/typography'
 
 export default function Home() {
   return (
     <FlexDiv centered>
       <Card className='w-96'>
         <CardHeader>
-          <h2>Sign in</h2>
+          <Typography.H3 className='text-center'>Sign in</Typography.H3>
         </CardHeader>
         <CardContent>
           <SignInForm />
